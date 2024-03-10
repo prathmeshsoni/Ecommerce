d3e1d171c5e760fb66f5e1ff0fa35a492b1cfd28
prathmesh <prathmesh.soni51@gmail.com>
2024-03-10 23:38:09 +0530

    Updated At 10-03-2024

--- music_club/settings.py
+++ music_club/settings.py
@@ -131,9 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
