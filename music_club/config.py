add1c1cc39eb94a2b5ba8fa02462044306c953fb
prathmesh <prathmesh.soni51@gmail.com>
2024-04-12 00:19:44 +0530

    Project

---
+++ music_club/config.py
@@ -0,0 +1,7 @@
+# For sending email to users during registration or password recovery
+email = ''
+password = ''
+
+# For integrating with the Razorpay payment gateway.
+razorpay_key_id = ''
+razorpay_key_secret = ''
---
+++ music_club/urls.py
@@ -0,0 +1,35 @@
+"""music_club URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path,include
+from django.conf import settings
+from django.contrib import admin
+from User.views import home
+from django.views.static import serve
+from django.conf.urls import url
+
+urlpatterns = [
+
+    path('', home ),
+    path('admin_side/', admin.site.urls),
+    path('user/', include('User.urls')),
+    path('admin/', include('Admin.urls')),
+    url(r'^uploads/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+
+]
+
+# +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "User.views.page_not_found_view"
